d76ef97ef1809e83cc37cfe9d0fe312a0d6cf9fc
Madeline Scyphers <madelinescyphers@gmail.com>
2025-11-21 01:11:36 -0700

    Update sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ pygments_style = None
 #
 # html_theme = 'alabaster'
 # html_theme = 'sphinx_nefertiti'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
