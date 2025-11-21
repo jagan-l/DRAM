7b664797798a774c7ef1eeecbad4acd2be4531aa
Madeline Scyphers <madelinescyphers@gmail.com>
2025-11-21 01:25:44 -0700

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -77,8 +77,9 @@ pygments_style = None
 #
 # html_theme = 'alabaster'
 # html_theme = 'sphinx_nefertiti'
-html_theme = 'sphinx_theme_pd'
-html_theme_path = [sphinx_theme_pd.get_html_theme_path()]
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
